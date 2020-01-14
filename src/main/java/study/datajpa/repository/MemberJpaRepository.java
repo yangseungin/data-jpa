9400189f7e73de060865931d59596ac09254f004
seungin yang <rhfpdk92@naver.com>
2020-01-15 08:40:04 +0900

    Make pure jpa repository

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -12,12 +14,31 @@ public class MemberJpaRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public Member save(Member member){
+    public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
-    public Member find(Long id){
-        return em.find(Member.class,id);
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public Long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
     }
 }
